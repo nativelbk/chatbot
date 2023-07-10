471388a7f70ead943151bea671e92da1af26b201
nativelbk <129794127+nativelbk@users.noreply.github.com>
2023-07-10 16:38:08 +0300

    Update webhook-controllers.js

--- controllers/webhook-controllers.js
+++ controllers/webhook-controllers.js
@@ -25,26 +25,27 @@ const post = async (req,res)=>{
     
     
     let body = req.body;
-    console.log(body)
+    console.log("body")
     console.log(`\u{1F7EA} Received webhook:`);
     console.dir(body, { depth: null });
 
     if (body.object === "page") {
         // Returns a '200 OK' response to all requests
-       try {
+       /*try {
         const data = await axios.post(`https://graph.facebook.com/16.0/${body.entry[0].messaging[0].recipient.id}/messages?recipient={'id':'${body.entry[0].messaging[0].sender.id}'}&messaging_type=RESPONSE&message={'text':'hello,world'}&access_token=${process.env.TOKEN}`)
         console.log(data)
        } catch (error) {
         console.log(error)
-       }
+       }*/
+        console.log("EVENT_RECEIVED")
        res.status(200).send("EVENT_RECEIVED");
     
         // Determine which webhooks were triggered and get sender PSIDs and locale, message content and more.
     
       } else {
         // Return a '404 Not Found' if event is not from a page subscription
-        console.log('not found')
-        res.sendStatus(200);
+        
+        res.sendStatus(404);
       }
 }
 
