4d1def21938dc139ed72e87ad805947f04517833
nativelbk <129794127+nativelbk@users.noreply.github.com>
2023-07-10 15:48:10 +0300

    Update webhooks-routes.js

--- routes/webhooks-routes.js
+++ routes/webhooks-routes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const {get,post} = require('../controllers/webhook-controllers')
 
-router.get('/messaging-webhook',get)
+router.get('/',get)
 router.post('/webhook',post)
 
 module.exports = router
