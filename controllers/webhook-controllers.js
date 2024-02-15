dc3b79b6af65d5781fbe75020c80a723a95076b1
Nativel <nativelbk@gmail.com>
2024-02-15 19:02:14 +0100

    cleaning the code

--- controllers/webhook-controllers.js
+++ controllers/webhook-controllers.js
@@ -6,17 +6,17 @@ const get = (req,res)=>{
     let mode = req.query["hub.mode"];
     let token = req.query["hub.verify_token"];
     let challenge = req.query["hub.challenge"];
-    
-    
+
   // Check if a token and mode is in the query string of the request
   if (mode && token) {
-    console.log('get request')
+
     // Check the mode and token sent is correct
     if (mode === "subscribe" && token === process.env.TOKEN) {
+
       // Respond with the challenge token from the request
-      console.log("WEBHOOK_VERIFIED");
       res.status(200).send(challenge);
     } else {
+
       // Respond with '403 Forbidden' if verify tokens do not match
       res.sendStatus(403);
     }
@@ -27,37 +27,29 @@ const get = (req,res)=>{
 const post = async (req,res)=>{
 
     let body = req.body
-    
     if (body.object === "page") {
-
         try {
-             const a = body.entry[0].messaging[0].recipient.id
-             const b = body.entry[0].messaging[0].sender.id
-            const corps = {
-            recipient:{
-            id:`${b}`
-            },
-            messaging_type: "RESPONSE",
-            message:{
+              const a = body.entry[0].messaging[0].recipient.id
+              const b = body.entry[0].messaging[0].sender.id
+              const corps = {
+              recipient:{
+              id:`${b}`
+              },
+              messaging_type: "RESPONSE",
+              message:{
               text:`${body.entry[0].messaging[0].message.text}(1)` 
             }
-
             }
             const data = await axios.post(`https://graph.facebook.com/v17.0/${a}/messages?access_token=${process.env.TOKEN}`,corps)
         } catch (error) {
         console.log(error)
        }
-
+       // Determine which webhooks were triggered and get sender PSIDs and locale, message content and more.
        res.status(200).send("EVENT_RECEIVED");
-    
-        // Determine which webhooks were triggered and get sender PSIDs and locale, message content and more.
-    
       } else {
         // Return a '404 Not Found' if event is not from a page subscription
-        
         res.sendStatus(404);
       }
-
 }
 
 module.exports = {get,post}
